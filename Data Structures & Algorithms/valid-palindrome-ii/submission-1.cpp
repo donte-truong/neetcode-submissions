536d7297d58d30df31a5ecdf6fcb174dc23c7432
Donte Truong <75056069+donte-truong@users.noreply.github.com>
2026-06-18 01:06:04 -0500

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return checkPalindrome(s, l + 1, r) || checkPalindrome(s, l, r - 1);
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+private:
+    bool checkPalindrome(string& s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
