055711c45f4937f3a481a2b44abaf024a2715bc3
Donte Truong <75056069+donte-truong@users.noreply.github.com>
2026-07-12 20:59:50 -0500

    Add: house-robber-iii - submission-1

---
+++ Data Structures & Algorithms/house-robber-iii/submission-1.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    pair<int, int> getMax(TreeNode* node) {
+        if (!node) return {0, 0};
+
+        pair<int, int> leftMax = getMax(node->left);
+        pair<int, int> rightMax = getMax(node->right);
+
+        return {
+            node->val + leftMax.second + rightMax.second,
+            max(leftMax.first, leftMax.second) + max(rightMax.first, rightMax.second)
+        };
+    }
+
+public:
+    int rob(TreeNode* root) {
+        pair<int, int> maxVal = getMax(root);
+
+        return max(maxVal.first, maxVal.second);
+    }
+};
