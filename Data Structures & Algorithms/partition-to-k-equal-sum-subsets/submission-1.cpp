4a2ea39dcae27e423abffbd1590375d6a145e1a7
Donte Truong <75056069+donte-truong@users.noreply.github.com>
2026-07-13 20:54:36 -0500

    Add: partition-to-k-equal-sum-subsets - submission-1

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    bool backtrack(vector<int>& nums, int i, vector<int>& subsets, int target) {
+        if (i == nums.size()) return true;
+
+        for (int j = 0; j < subsets.size(); j++) {
+            if (subsets[j] + nums[i] > target) continue;
+
+            subsets[j] += nums[i];
+            if (backtrack(nums, i + 1, subsets, target)) return true;
+            subsets[j] -= nums[i];
+
+            if (subsets[j] == 0) return false;
+        }
+
+        return false;
+    }
+
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if (total % k != 0) return false;
+
+        sort(nums.begin(), nums.end(), greater<>());
+        
+        vector<int> subsets(k, 0);
+        return backtrack(nums, 0, subsets, total / k);
+
+    }
+};
