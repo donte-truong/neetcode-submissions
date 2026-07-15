6be6193da0f7e88cd8818498a2f454f8d2ecc10a
Donte Truong <75056069+donte-truong@users.noreply.github.com>
2026-07-15 15:15:32 -0500

    Add: n-queens-ii - submission-1

---
+++ Data Structures & Algorithms/n-queens-ii/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    vector<bool> cols;
+    vector<bool> diag1;
+    vector<bool> diag2;
+
+    int solutions = 0;
+
+    void backtrack(int row) {
+        if (row == cols.size()) {
+            solutions += 1;
+            return;
+        }
+
+        for (int col = 0; col < cols.size(); col++) {
+            int d1 = row + col;
+            int d2 = row - col + cols.size() - 1;
+
+            if (cols[col] || diag1[d1] || diag2[d2]) continue;
+
+            cols[col] = diag1[d1] = diag2[d2] = true;
+            backtrack(row + 1);
+            cols[col] = diag1[d1] = diag2[d2] = false;
+        }
+    }
+
+
+public:
+    int totalNQueens(int n) {
+        cols.assign(n, false);
+        diag1.assign(2 * n - 1, false);
+        diag2.assign(2 * n - 1, false);
+
+        backtrack(0);
+
+        return solutions;
+    }
+};
