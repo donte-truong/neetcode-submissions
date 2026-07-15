be5739d6186f3a4b319d4034f1c5c16521df558f
Donte Truong <75056069+donte-truong@users.noreply.github.com>
2026-07-15 16:23:53 -0500

    Add: longest-happy-string - submission-2

---
+++ Data Structures & Algorithms/longest-happy-string/submission-2.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        priority_queue<pair<int, char>> maxHeap;
+
+        if (a) maxHeap.push({a, 'a'});
+        if (b) maxHeap.push({b, 'b'});
+        if (c) maxHeap.push({c, 'c'});
+
+        string ans = "";
+        pair<char, char> prev = {'0', '0'};
+
+        while (!maxHeap.empty()) {
+
+            auto [count, c] = maxHeap.top();
+            maxHeap.pop();
+
+            if (c == prev.first && c == prev.second) {
+                if (maxHeap.empty()) return ans;
+
+                auto [nextCount, nextC] = maxHeap.top();
+                maxHeap.pop();
+
+                ans += nextC;
+                if (--nextCount) maxHeap.push({nextCount, nextC});
+                prev.second = nextC;
+            }
+
+            ans += c;
+            if (--count) maxHeap.push({count, c});
+
+            prev.first = prev.second;
+            prev.second = c;
+
+        }
+
+        return ans;
+    }
+};
