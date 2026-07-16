5b64b3a388ee1adac3da8a20f7af6095aa11c43e
Donte Truong <75056069+donte-truong@users.noreply.github.com>
2026-07-16 15:00:47 -0500

    Add: open-the-lock - submission-1

---
+++ Data Structures & Algorithms/open-the-lock/submission-1.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int openLock(vector<string>& deadends, string target) {
+        unordered_set<string> invalid;
+        for (string& deadend : deadends) {
+            invalid.insert(deadend);
+        }
+
+        priority_queue<pair<int, string>> combinations;
+        combinations.push({0, "0000"});
+
+        unordered_map<string, int> costMap;
+        costMap["0000"] = 0;
+
+        while (!combinations.empty()) {
+            auto [cost, combination] = combinations.top();
+            combinations.pop();
+
+            if (combination == target)
+                return -cost;
+
+            if (invalid.contains(combination))
+                continue;
+
+            for (int i = 0; i < 4; ++i) {
+                string up = combination;
+                up[i] = ((up[i] - '0' + 1) % 10) + '0';
+
+                if (!costMap.contains(up) || -cost + 1 < costMap[up]) {
+                    costMap[up] = -cost + 1;
+                    combinations.push({cost - 1, up});
+                }
+
+                string down = combination;
+                down[i] = ((down[i] - '0' + 9) % 10) + '0';
+
+                if (!costMap.contains(down) || -cost + 1 < costMap[down]) {
+                    costMap[down] = -cost + 1;
+                    combinations.push({cost - 1, down});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
