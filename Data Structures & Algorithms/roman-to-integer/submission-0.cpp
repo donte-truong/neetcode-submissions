0f9ce9b68f91d8b2cfb8d0467c9b4db6e82fe255
Donte Truong <75056069+donte-truong@users.noreply.github.com>
2026-07-25 12:04:02 -0500

    Add: roman-to-integer - submission-0

---
+++ Data Structures & Algorithms/roman-to-integer/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> valueMap = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000},
+            {' ', 0}
+        };
+        
+        int value = 0;
+        char prev = ' ';
+        for (char c : s) {
+            if (
+                (prev == 'I' && (c == 'V' || c == 'X')) ||
+                (prev == 'X' && (c == 'L' || c == 'C')) ||
+                (prev == 'C' && (c == 'D' || c == 'M'))
+            ) {
+                value -= 2 * valueMap[prev];
+            }
+            prev = c;
+            value += valueMap[c];
+        }
+        
+        return value;
+    }
+};
