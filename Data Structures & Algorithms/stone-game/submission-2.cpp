fd230fb8c025e03567686c8d44abfd7067cb05f6
Donte Truong <75056069+donte-truong@users.noreply.github.com>
2026-07-27 22:10:58 -0500

    Add: stone-game - submission-2

---
+++ Data Structures & Algorithms/stone-game/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        vector<int> dp(piles.size(), 0);
+
+        for (int l = piles.size() - 1; l >= 0; --l) {
+            vector<int> nextDp = dp;
+            nextDp[l] = piles[l];
+            for (int r = l + 1; r < piles.size(); ++r) {
+                nextDp[r] = max(piles[r] - nextDp[r - 1], piles[l] - dp[r]);
+            }
+            swap(nextDp, dp);
+        }
+
+        return dp[piles.size() - 1] > 0;
+    }
+};
